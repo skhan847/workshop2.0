87e7ba274386392e18fb847ac4eba59fd67bda69
skhan847 <skhan847@outlook.com>
2022-03-04 19:18:23 -0600

    switch

--- app/nc_tutorials/settings.py
+++ app/nc_tutorials/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-(j-#aqq@vs$3l#0(031z4_h(_*pa&ye#dt&x69_@(&a6(=+fpg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ec2-44-201-83-52.compute-1.amazonaws.com', '0.0.0.0', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['ec2-52-91-160-140.compute-1.amazonaws.com:8000', '0.0.0.0', 'localhost', '127.0.0.1']
 
 
 # Application definition
